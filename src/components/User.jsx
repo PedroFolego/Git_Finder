eda430de72e652fff67563c2fe1a20fa59ed4ce5
Pedro Folego <hmfpedro@hotmail.com>
2022-08-04 10:22:03 -0300

    feat: style user component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,37 +1,46 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function User({ dataProfile }) {
   const {
     twitter_username: twitterUsername,
-    public_repos: publicRepos,
     avatar_url: avatarUrl,
     company, blog, location,
     following, name, bio,
     login, followers,
   } = dataProfile;
   return (
-    <section>
-      <img src={avatarUrl} alt="user profile" />
-      <h1>{name}</h1>
-      <h3>{login}</h3>
-      <h3>{bio}</h3>
-      <h3>{blog}</h3>
-      <h3>{twitterUsername}</h3>
-      <h3>{publicRepos}</h3>
-      <h3>{company}</h3>
-      <h3>{location}</h3>
-      <h3>
-        {following}
-        {' '}
-        following
-      </h3>
-      <h3>
-        {followers}
-        {' '}
-        followers
-      </h3>
-    </section>
+    <aside className="w-50 p-4 d-flex flex-column justify-content-center align-items-center">
+      <img src={avatarUrl} alt="user profile" className="rounded-circle w-75 p-3" />
+      <article className="d-flex flex-column">
+        <h2>{name}</h2>
+        <h4 className="mb-4">{login}</h4>
+        <Button variant="secondary" className="d-flex align-self-center w-50 justify-content-center">Follow</Button>
+        <h5 className="my-4">{bio}</h5>
+        <h5>{blog}</h5>
+        {twitterUsername && (
+        <h5>
+          @
+          {twitterUsername}
+        </h5>
+        )}
+        <h5>{company}</h5>
+        <h5>{location}</h5>
+        <div className="d-flex flex-row justify-content-around align-items-center">
+          <p>
+            <b>{following}</b>
+            {' '}
+            following
+          </p>
+          <p>
+            <b>{followers}</b>
+            {' '}
+            followers
+          </p>
+        </div>
+      </article>
+    </aside>
   );
 }
 
