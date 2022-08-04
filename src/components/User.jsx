0d0ac316d46ebd846ea127eed26d17f615bcdaf9
Pedro Folego <hmfpedro@hotmail.com>
2022-08-03 22:19:05 -0300

    feat: add proptypes User

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,34 @@
-function User(dataUser) {
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function User(dataProfile) {
+  const {
+    twitter_username: twitterUsername,
+    public_repos: publicRepos,
+    avatar_url: avatarUrl,
+    company, blog, location,
+    following, name, bio,
+    login, followers,
+  } = dataProfile;
   return (
     <section />
   );
 }
 
 export default User;
+
+User.propTypes = {
+  dataProfile: PropTypes.shape({
+    name: PropTypes.string,
+    avatarUrl: PropTypes.string,
+    company: PropTypes.string,
+    blog: PropTypes.string,
+    location: PropTypes.string,
+    bio: PropTypes.string,
+    twitterUsername: PropTypes.string,
+    login: PropTypes.string,
+    followers: PropTypes.number,
+    following: PropTypes.number,
+    publicRepos: PropTypes.number,
+  }),
+}.isRequired;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,14 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
+import User from '../components/User';
 
 function UserProfile({ dataProfile }) {
-  const {
-    name, bio, twitter_username,
-    avatar_url, repos_url,
-    company, blog, location,
-    login, followers, following,
-    public_repos,
-  } = dataProfile;
+  const { repos_url: reposUrl } = dataProfile;
   useEffect(() => {
 
   });
@@ -17,8 +12,8 @@ function UserProfile({ dataProfile }) {
   // };
   return (
     <main>
-      <h1>Profile</h1>
-      <h3>{dataProfile.name}</h3>
+      <User dataProfile={dataProfile} />
+      <h1>{reposUrl}</h1>
     </main>
   );
 }
@@ -27,17 +22,6 @@ export default UserProfile;
 
 UserProfile.propTypes = {
   dataProfile: PropTypes.shape({
-    name: PropTypes.string,
-    avatar_url: PropTypes.string,
     repos_url: PropTypes.string,
-    company: PropTypes.string,
-    blog: PropTypes.string,
-    location: PropTypes.string,
-    bio: PropTypes.string,
-    twitter_username: PropTypes.string,
-    login: PropTypes.string,
-    followers: PropTypes.number,
-    following: PropTypes.number,
-    public_repos: PropTypes.number,
   }),
 }.isRequired;
